5a46454356550a19b9c5418a1e03e70864b7e1c7
Nicholas Schweitzer <f3bl@gmx.com>
2020-04-29 09:57:08 +0200

    seperate bin directory

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gcc -g -o demo/triangle $(pkg-config --cflags --libs vulkan) $(pkg-config --cflags --libs glfw3) src/* demo/triangle.c
+gcc -g -o bin/triangle $(pkg-config --cflags --libs vulkan) $(pkg-config --cflags --libs glfw3) src/* demo/triangle.c
 
-test $? -eq 0 && demo/triangle
+test $? -eq 0 && bin/triangle
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gcc -g -o test-runner/runner $(pkg-config --cflags --libs vulkan) $(pkg-config --cflags --libs glfw3) $(pkg-config --cflags --libs check) src/* tests-src/* test-runner/runner.c
+gcc -g -o bin/runner $(pkg-config --cflags --libs vulkan) $(pkg-config --cflags --libs glfw3) $(pkg-config --cflags --libs check) src/* tests-src/* test-runner/runner.c
 
-test $? -eq 0 && test-runner/runner
+test $? -eq 0 && bin/runner
